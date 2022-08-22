4f10318721e56a5074e8a52cfe54d19ea45d6130
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-22 05:18:13 +0000

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Auth endpoint");
-});
+router.get("/register", register);
 
 export default router;
