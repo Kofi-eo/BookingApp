94eaea77c21f5269a68f4aa5320e040269f34381
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-22 04:40:16 +0000

    Update hotels.js

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,9 +1,37 @@
 import express from "express";
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+} from "../controllers/hotels.js";
+import Hotel from "../models/hotels.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hotel endpoint");
+//CREATE
+router.post("/", createHotel);
+
+//UPDATE
+router.put("/:id", updateHotel);
+
+//DELETE
+router.delete("/:id", deleteHotel);
+
+//GET
+router.get("/:id", getHotel);
+
+//GET ALL
+router.get("/", async (req, res) => {
+  const failed = true;
+  if (failed) return next(createError(401, "You are not Authenticated"));
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
