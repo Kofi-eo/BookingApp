f783efe6180409225bfca014644009474093d145
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-22 04:40:51 +0000

    Create hotels.js

---
+++ api/controllers/hotels.js
@@ -0,0 +1,64 @@
+export const createHotel = async (req, res, next) => {
+  router.post("/", async (req, res) => {
+    const newHotel = new Hotel(req.body);
+
+    try {
+      const savedHotel = await newHotel.save();
+      res.status(200).json(savedHotel);
+    } catch (err) {
+      next(err);
+    }
+  });
+};
+
+export const updateHotel = async (req, res, next) => {
+  router.put("/:id", async (req, res) => {
+    try {
+      const updatedHotel = await Hotel.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(savedHotel);
+    } catch (err) {
+      next(err);
+    }
+  });
+};
+
+export const deleteHotel = async (req, res, next) => {
+  router.delete("/:id", async (req, res) => {
+    try {
+      await Hotel.findByIdAndDelete(req.params.id);
+      res.status(200).json("Hotel has been deleted");
+    } catch (err) {
+      next(err);
+    }
+  });
+};
+
+export const getHotel = async (req, res, next) => {
+  router.get("/:id", async (req, res) => {
+    try {
+      const hotel = await Hotel.findByIdAndUpdate(req.params.id);
+      res.status(200).json(hotel);
+    } catch (err) {
+      next(err);
+    }
+  });
+};
+
+export const getAllHotel = async (req, res, next) => {
+  router.post("/", async (req, res) => {
+    const newHotel = new Hotel(req.body);
+
+    try {
+      const savedHotel = await newHotel.save();
+      res.status(200).json(savedHotel);
+    } catch (err) {
+      next(err);
+    }
+  });
+};
