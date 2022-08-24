4229c13adcfb902b6365621a55d6f99571c6bcd9
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-24 13:13:48 +0000

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,34 @@
 import express from "express";
+import {
+  createUser,
+  updateUser,
+  deleteUser,
+  getUser,
+  getAllUser,
+} from "../controllers/users.js";
+import User from "../models/users.js";
+import { createError } from "../utils/error.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("User endpoint");
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("Hello User, you are logged in");
 });
+
+//CREATE
+router.post("/", createUser);
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getAllUser);
 
 export default router;
